dd3cb284c830ddf06187c0cb3cfe67fd36aef6a8
Spartan-F117 <58399903+Spartan-F117@users.noreply.github.com>
2023-03-29 16:16:46 +0200

    Delete OAI directory

--- OAI/Script/runOAI.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd ..
-sudo sysctl net.ipv4.conf.all.forwarding=1
-sudo iptables -P FORWARD ACCEPT
-cd oai-cn5g-fed/
-cd docker-compose/
-sudo python3 core-network.py --type start-basic-vpp
-sudo docker ps
--- OAI/Script/stopUERANSIM.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd ..
-cd oai-cn5g-fed/
-cd docker-compose/
-sudo docker-compose -f docker-compose-ueransim-vpp.yaml down
-sudo docker ps
